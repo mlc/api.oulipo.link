ce33abad0157d88537e4616951206c141db1a26c
mike castleman <m@mlcastle.net>
2023-03-29 02:31:13 -0400

    switch building tool

---
+++ build.mjs
@@ -0,0 +1,18 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+  outdir: 'dist',
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: false,
+  sourcemap: true,
+  write: true,
+  platform: 'node',
+  external: ['@aws-sdk'],
+  metafile: true,
+  target: 'node18',
+  format: 'esm',
+  outExtension: {
+    '.js': '.mjs',
+  },
+});
